f976acf50da187c01dba2b80d05cfd580f945472
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-26 21:13:32 +0530

    m

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "example" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = aws_db_parameter_group.default
+  parameter_group_name = aws_db_parameter_group.default.name
   engine_version       = "6.2"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.roboshopsubnetGroup.name
