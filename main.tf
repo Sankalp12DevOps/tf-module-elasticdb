8cc024132e1d358b5549433962dde0eed8dba538
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-26 22:31:49 +0530

    m

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ resource "aws_elasticache_cluster" "example" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = aws_db_parameter_group.default.name
+  parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.roboshopsubnetgroup.name
   security_group_ids   = [aws_security_group.allow_tls-robosop_elastiCache.id]
 }
 
-resource "aws_db_parameter_group" "default" {
+resource "aws_elasticache_parameter_group" "default" {
   name   = "${var.ENV}-elastcache-paramgroup"
   family = "redis6.x"
 }
